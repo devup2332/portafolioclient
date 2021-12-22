5a21221fc43406ff6b4df87c99624cfd3d80db62
Diego Rojas <devup2332@gmail.com>
2021-12-22 12:45:53 -0500

    release: Aded meta to allow insecure http calls

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -12,6 +12,7 @@ const Base = ({ header, section, children }: BaseProps) => {
         <div>
             <Head>
                 <title>Drojas - {section}</title>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;500;600&family=Roboto:wght@100;300;400;500;700;900&display=swap"
                     rel="stylesheet"
