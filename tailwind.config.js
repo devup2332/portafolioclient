eacc31ebed28adf8fe26e77bb5cb56a71227860d
Diego Rojas <devup2332@gmail.com>
2022-01-26 03:07:29 -0500

    develop: Added create project page to admin section UPP-03: Merged 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,70 @@
 module.exports = {
+<<<<<<< HEAD
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
       robotoMono: ["Roboto Mono", "monospace"],
+=======
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            roboto: ["Roboto", "sans-serif"],
+            robotoMono: ["Roboto Mono", "monospace"],
+        },
+        colors: {
+            primary: "#031927",
+            secondary: "#D10000",
+            "button-2": "#E7E9EA",
+            "black-transparent": "rgba(0,0,0,0.3)",
+            card: "#F6F6F6",
+            white: "#ffffff",
+            black: "#000000",
+            hover: "#E7E7E7",
+            danger: "#e31717",
+        },
+        boxShadow: {
+            sm: "0px 5px 5px 0px rgba(0,0,0,0.3)",
+            xl: "0px 5px 5px 0px rgba(0,0,0,0.5)",
+        },
+        fontSize: {
+            xs: "13px",
+            base: "14px",
+            "2xl": "20px",
+            "3xl": "25px",
+            "4xl": "30px",
+            "5xl": "45px",
+            "7xl": "65px",
+            "8xl": "90px",
+            lg: "20px",
+        },
+        maxHeight: {
+            90: "90vh",
+            225: "225px",
+            500: "500px",
+        },
+        maxWidth: {
+            300: "300px",
+            330: "330px",
+            400: "400px",
+            700: "700px",
+            1000: "1000px",
+            1150: "1150px",
+            1180: "1180px",
+            1300: "1300px",
+            1500: "1500px",
+        },
+        minWidth: {
+            280: "280px",
+        },
+        minHeight: {
+            300: "300px",
+            400: "400px",
+        },
+        extend: {},
+>>>>>>> c3235ea (develop: Added create project page to admin section)
     },
     colors: {
       primary: "#031927",
