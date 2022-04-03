f6e323ace58b73b1e9c8b3ef5b29453ed6c5290d
Diego Rojas <devup2332@gmail.com>
2022-04-03 11:02:02 -0500

    UPP-03: Added redux toolkit to manage global state UPP-03: Merge 6

--- src/containers/createNewProjectContainer.tsx
+++ src/containers/createNewProjectContainer.tsx
@@ -34,6 +34,7 @@ const CreateNewProjectContainer = () => {
   const createProject = async (data: any) => {
     try {
       setLoading(true);
+      console.log("DATA", data);
       await createProjectRest(data, user?.id);
 
       setSelectOptions(
