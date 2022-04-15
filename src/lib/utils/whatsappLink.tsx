501a68df9ef045fb7e9b8f40f24335de99ed7bd1
Diego Rojas <devup2332@gmail.com>
2022-04-15 03:13:14 -0500

    release0.5.1: Added ? sign to whatsapp message

--- src/lib/utils/whatsappLink.tsx
+++ src/lib/utils/whatsappLink.tsx
@@ -1,7 +1,7 @@
 export const whatsappLink = (message: string, phone?: number | string | undefined, isMobile?: boolean) => {
     let url = isMobile ? "https://wa.me" : "https://web.whatsapp.com/send";
     if (phone) {
-        url += isMobile ? `/${phone}` : `?phone=51${phone}&`;
+        url += isMobile ? `/${phone}?` : `?phone=51${phone}&`;
     } else {
         url += "?";
     }
